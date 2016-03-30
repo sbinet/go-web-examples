22952bd48b9f37cd9195f49db2a4d41038b1a3a3
Sebastien Binet <binet@cern.ch>
2016-03-30 20:04:10 +0200

    06-go-web-gopherjs: tweak countdown

--- 06-go-web-gopherjs/root-fs/app.go
+++ 06-go-web-gopherjs/root-fs/app.go
@@ -15,23 +15,25 @@ func tickTxt(t time.Duration) string {
 }
 
 func main() {
-	max := 3 * time.Second
+	max := 5 * time.Second
 	doc := js.Global.Get("document")
 	doc.Get("body").Set("innerHTML", tickTxt(max))
 	start := time.Now()
 	timeout := time.After(max)
-	ticker := time.NewTicker(100 * time.Millisecond)
+	ticker := time.NewTicker(10 * time.Millisecond)
+
 loop:
 	for {
 		select {
 		case <-timeout:
+			ticker.Stop()
 			break loop
 		case c := <-ticker.C:
 			doc.Get("body").Set("innerHTML", tickTxt(max-c.Sub(start)))
 		}
 	}
 	str := "you"
-	doc.Get("body").Set("innerHTML", pkg.Hello(str)+"\n<p>An <em>alert</em> should pop up in 5s</p>")
-	<-time.After(5 * time.Second)
+	doc.Get("body").Set("innerHTML", pkg.Hello(str)+"\n<p>An <em>alert</em> should pop up in 3s</p>")
+	<-time.After(3 * time.Second)
 	js.Global.Call("alert", pkg.Hello(str))
 }
