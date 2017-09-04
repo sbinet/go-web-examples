d67eba24f92da37228af176e34d05d5b6bcf505f
Sebastien Binet <binet@cern.ch>
2017-09-04 10:43:54 +0200

    07-go-web-plot-gonum-svg: migrate to gonum.org/v1/plot

--- 07-go-web-plot-gonum-svg/main.go
+++ 07-go-web-plot-gonum-svg/main.go
@@ -10,13 +10,13 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gonum/plot"
-	"github.com/gonum/plot/plotter"
-	"github.com/gonum/plot/vg"
-	"github.com/gonum/plot/vg/draw"
-	"github.com/gonum/plot/vg/vgsvg"
-
 	"golang.org/x/net/websocket"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot/vg/vgsvg"
 )
 
 var (
